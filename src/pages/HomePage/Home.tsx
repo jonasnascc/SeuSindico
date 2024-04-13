1230379f67de45c66d4a76159e1e2f96e77c1293
Jonas Nascimento <jonashnascimento@gmail.com>
2024-04-13 13:58:24 -0300

    corrigir erro de imports de componentes deletados

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
-import { PageContainer } from "./styles";
-import { PagesList } from "../../shared/components/HomePagesList/PagesList";
+
 
 export const HomePage = () => {
     return (
